b1ef14d043ec7af5928506fada35f49abd422098
fairydream <fairydream523@126.com>
2015-01-30 19:31:53 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by fairydream on 15-1-30.
+//  Copyright (c) 2015年 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Gameplay : CCSprite
+
+@end
