e2ed1c35d21c93d46893b7fed912abc4234b3dcc
fairydream <fairydream523@126.com>
2015-02-03 07:38:11 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by fairydream on 15-2-3.
+//  Copyright (c) 2015年 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
